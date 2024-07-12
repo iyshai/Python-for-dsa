7605b74e3243c362281303e2a79d9c957939f21e
iyshai <yuv450@gmail.com>
2024-07-13 02:07:36 +0530

    1-commit

--- Day01.py
+++ Day01.py
@@ -1,1 +1,13 @@
-print("this is my first py program")
+# print("this is my first py program\nhello world\nnew world ")
+# print("hello"+" "+"world") #this is string concatenation  
+# name=input("what is your name? ") #this is the input function
+# print("my name is: "+name)
+
+
+name =input("what is your name?")
+lengthw =len(name)
+print (lengthw);
+# print ("hello " +input("what is your name?"))
+
+
+
