e9e5c9582379ef4ed78cb5a774ea512fd34c1752
iyshai <yuv450@gmail.com>
2024-07-15 00:24:05 +0530

    Update Day01.py

--- Day01.py
+++ Day01.py
@@ -3,11 +3,34 @@
 # name=input("what is your name? ") #this is the input function
 # print("my name is: "+name)
 
+# nxt
 
-name =input("what is your name?")
-lengthw =len(name)
-print (lengthw);
-# print ("hello " +input("what is your name?"))
+# lengthw =len(name)
+# print (lengthw);
+# # print ("hello " +input("what is your name?"))
+
+# # nxt
+
+# print(len(input("what is your name")))
+
+# # nxt
+
+# name =input("what is your name?")
+
+# nxt
+
+a= input("enter the value of a :")
+b= input("enter the value of b :")
+
+temp1=a
+temp2=b
+
+c=a
+a=b
+
+a=temp2
+b=temp1
 
 
-
+print("value of a: "+ a)
+print("value of b:" + b)
